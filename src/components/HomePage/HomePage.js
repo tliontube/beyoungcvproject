4fa0025d50404071c4c135a11dc06ffde1a30a80
tliontube <thomaswizfan@gmail.com>
2023-09-29 23:26:41 +0530

    version 1

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -30,8 +30,32 @@ function HomePage() {
         </div>
       </section>
       <section>
-        <span id={slide && "dark_dot"} className="dot" onClick={handleClick}></span>
-        <span id={!slide && "dark_dot"} className="dot" onClick={handleClick}></span>
+        <span
+          id={slide && "dark_dot"}
+          className="dot"
+          onClick={handleClick}
+        ></span>
+        <span
+          id={!slide && "dark_dot"}
+          className="dot"
+          onClick={handleClick}
+        ></span>
+      </section>
+      <section className="Home__page--ads__Parent">
+        <section className="Home__page--ads">
+          <a className="Home__page--ads__1">
+            <img
+              src="https://www.beyoung.in/api/catalog/homepage-05-06-23/Travel-Campaign.jpg"
+              alt="add Photo"
+            />
+          </a>
+          <a className="Home__page--ads__2">
+            <img
+              src="https://www.beyoung.in/api/catalog/home-19-7-23/shop-the-look-home-page-section-f.jpg"
+              alt="add Photo"
+            />
+          </a>
+        </section>
       </section>
     </section>
   );
