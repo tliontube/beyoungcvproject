b91e9f6ab177a81021e5b5911fac3fd1dfc6130c
tliontube <thomaswizfan@gmail.com>
2023-09-29 21:55:49 +0530

    version 1

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./HomePage1.css";
+function HomePage() {
+  const [slide, setSlide] = useState(true);
 
-function HomePage(){
-    return(
-        <section className="Home__Page--slider">
-            <div className="My__slide">
-            <img src="https://www.beyoung.in/api/catalog/home-19-8-23/new/combo-banner-desktop-vieww1.jpg"/>
-            </div>
-            <div className="My__slide">
-            <img src="https://www.beyoung.in/api/catalog/home-19-8-23/new/Boxer-home-page-banner-desktop-view1.jpg"/>
-            </div>
-        </section>
-    )
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSlide((prevSlide) => !prevSlide);
+    }, 6000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  function handleClick() {
+    setSlide((prevSlide) => !prevSlide);
+  }
+  return (
+    <section className="Home__Page--slider">
+      <section className="My__Slide--section">
+        <div className={slide && "My__slide"}>
+          <img
+            className="slide__img1 fade"
+            src="https://www.beyoung.in/api/catalog/home-19-8-23/new/combo-banner-desktop-vieww1.jpg"
+          />
+        </div>
+        <div className={!slide && "My__slide"}>
+          <img
+            className="slide__img2 fade"
+            src="https://www.beyoung.in/api/catalog/home-19-8-23/new/Boxer-home-page-banner-desktop-view1.jpg"
+          />
+        </div>
+      </section>
+      <section>
+        <span id={slide && "dark_dot"} className="dot" onClick={handleClick}></span>
+        <span id={!slide && "dark_dot"} className="dot" onClick={handleClick}></span>
+      </section>
+    </section>
+  );
 }
 
 export default HomePage;
